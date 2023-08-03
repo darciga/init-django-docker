2f09ce1851c244e667e133b7fa45cd3b6b812e0b
darciga <darciga@tingsystems.com>
2023-08-02 21:38:22 -0600

    change fix entry-point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py migrate                  # Apply database migrations
 # Start Gunicorn processes
 echo Starting server
   if [ "$DEBUG" = "1" ]; then
-    exec uvicorn api.asgi:application \
+    exec uvicorn todo.asgi:application \
       --log-level=info --host=0.0.0.0 --port=8080 \
       --workers 1
   else
