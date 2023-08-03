78c79e4b6e91d959e7b75491d9bd92ad97657305
darciga <darciga@tingsystems.com>
2023-08-02 21:33:44 -0600

    change requirements and set new db config

--- todo/settings.py
+++ todo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.environ.get('POSTGRES_NAME'),
+       'NAME': os.environ.get('POSTGRES_USER'),
        'USER': os.environ.get('POSTGRES_USER'),
        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
        'HOST': os.environ.get('POSTGRES_HOST'),
